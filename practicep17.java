3b88b0ddf9463c69248b95675a075828d7dcc789
rohandigrase12 <128697831+rohandigrase12@users.noreply.github.com>
2023-03-29 00:12:28 +0530

    Add files via upload

---
+++ practicep17.java
@@ -0,0 +1,9 @@
+
+class PracticeG17
+{
+public static void main(String args[])
+{
+    long l=9359785616;
+   System.out.println(l);
+}
+}
---
+++ practicep18.java
@@ -0,0 +1,9 @@
+
+class PracticeG18
+{
+public static void main(String args[])
+{
+    long l=9359785616l;
+   System.out.println(l);
+}
+}
